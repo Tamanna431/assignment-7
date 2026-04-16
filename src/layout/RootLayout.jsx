ca5af32801b9d54122e1413619188c11b45b572a
Tamanna431 <tamannashuchi06@gmail.com>
2026-04-17 00:55:31 +0600

    spelling correct

--- src/layout/RootLayout.jsx
+++ src/layout/RootLayout.jsx
@@ -2,19 +2,19 @@ import React from 'react';
 import { Outlet,useLocation} from 'react-router-dom'
 
 import { HomeIcon } from 'lucide-react';
-import Navbar from '../componenets/shared/Navbar'
-import BottomNav from '../componenets/shared/BottomNav';
-import Footer from "../componenets/shared/Footer";
+import Navbar from '../components/shared/Navbar'
+import BottomNav from '../components/shared/BottomNav';
+import Footer from "../components/shared/Footer";
 import { ToastContainer } from 'react-toastify';
 import "react-toastify/dist/ReactToastify.css";
 
 const RootLayout = () => {
      const location = useLocation();
-    const showNavbar = location.pathname !== 'friend/:id';
+    const showNavbar = location.pathname === 'friend/:id';
     const showBottom = location.pathname !== '/';
     return (
         <div>
-            {showNavbar && <Navbar />}
+            {showNavbar || <Navbar />}
             <Outlet/>
             {showBottom || <BottomNav/>}
             <Footer/>
